ad580a0fec9fb09f85b64528e5566251788124fb
VGLoic <vincent.genod.loic@gmail.com>
2023-11-19 19:39:54 +0900

    add command level

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::{env, process};
 
-use advent_of_code::Exercise;
+use advent_of_code::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let exercise = Exercise::try_from(&args).unwrap_or_else(|err| {
+    let command = Command::try_from(&args).unwrap_or_else(|err| {
         println!("Problem parsing argumments {err}");
         process::exit(1);
     });
 
-    if let Err(err) = exercise.run() {
+    if let Err(err) = command.run() {
         println!("Problem while running the exercise {err}");
         process::exit(1);
     }
